c26f7e72a0c092ba78a4c0090dc7265e8dcc103e
“kamranabbasi” <kamranabbasi@telenorbank.pk>
2024-06-17 10:00:26 +0100

    jpa repository has been added to communicate with database

---
+++ rent-properties-api/src/main/java/fr/esgi/repository/RentalCarRepository.java
@@ -0,0 +1,20 @@
+package fr.esgi.repository;
+
+import fr.esgi.domain.RentalPropertyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RentalCarRepository extends JpaRepository<RentalPropertyEntity, Integer> {
+    @Override
+    List<RentalPropertyEntity> findAll();
+
+    @Override
+    Optional<RentalPropertyEntity> findById(Integer id);
+
+    @Override
+    RentalPropertyEntity saveAndFlush(RentalPropertyEntity rentalProperty);
+}
